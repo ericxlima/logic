bc115d4e850e9f1fb4efa8adf7a797a088b3d732
Eric de Lima <eric.vinlima@gmail.com>
2023-11-02 15:45:08 -0300

    feat: add data by json

--- main.js
+++ main.js
@@ -1,3 +1,39 @@
+// Carregar o JSON com os dados dos capítulos
+fetch('data.json')
+  .then(response => response.json())
+  .then(data => {
+    const accordionBody = document.querySelector('.accordion-body');
+
+    for (const capitulo in data) {
+      if (data.hasOwnProperty(capitulo)) {
+        const capituloData = data[capitulo];
+
+        const cardDiv = document.createElement('div');
+        cardDiv.classList.add('flashcard');
+
+        cardDiv.innerHTML = `
+          <div class="flashcard__inner">
+            <div class="flashcard__face flashcard__face--front">
+              <h2>${capituloData.front}</h2>
+            </div>
+            <div class="flashcard__face flashcard__face--back">
+              <div class="flashcard__content">
+                <div class="flashcard__header">
+                  <h2>${capituloData.back}</h2>
+                </div>
+              </div>
+            </div>
+          </div>
+        `;
+
+        accordionBody.appendChild(cardDiv);
+      }
+    }
+  })
+  .catch(error => {
+    console.error('Erro ao carregar os dados do JSON:', error);
+  });
+
 const flashcards = document.querySelectorAll(".flashcard__inner");
 
 flashcards.forEach((flashcard) => {
